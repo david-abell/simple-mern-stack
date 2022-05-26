3186083bf8a5900d8382ccfd5d9a0c86d749614c
David Abell <david.abell.m@gmail.com>
2022-05-26 10:29:34 +0100

    create react app cleanup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
 
-function App() {
+// We use Route in order to define the different routes of our application
+import { Route, Routes } from "react-router-dom";
+
+// We import all the components we need in our app
+import Navigation from "./components/Navigation";
+import RecordList from "./components/RecordList";
+import Edit from "./components/Edit";
+import Create from "./components/Create";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-xxl mx-auto">
+      <Navigation />
+      <Routes>
+        <Route exact path="/" element={<RecordList />} />
+        <Route path="/edit/:id" element={<Edit />} />
+        <Route path="/create" element={<Create />} />
+      </Routes>
     </div>
   );
-}
+};
 
 export default App;
